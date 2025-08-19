aeebc5de22d68bb5d06055e026a38b758b822d7f
sai teja <147785419+Saiteja8466@users.noreply.github.com>
2025-08-20 04:16:43 +0530

    Create views.py

---
+++ employee_project/attendance/views.py
@@ -0,0 +1,10 @@
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Attendance
+from .serializers import AttendanceSerializer
+
+class AttendanceViewSet(viewsets.ModelViewSet):
+    queryset = Attendance.objects.all()
+    serializer_class = AttendanceSerializer
+    permission_classes = [IsAuthenticated]
+    filterset_fields = ['employee__name', 'date', 'status']
