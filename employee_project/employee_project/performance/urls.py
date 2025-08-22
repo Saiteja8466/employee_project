dcab9f2fcdef8712650b74d4499b77b015d134ae
sai teja <147785419+Saiteja8466@users.noreply.github.com>
2025-08-22 05:05:40 +0000

    Added Django project and apps: employees, attendance, departments, performance

---
+++ employee_project/employee_project/performance/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("charts/", views.performance_chart, name="performance-chart"),
+]
--- employee_project/urls.py
+++ employee_project/urls.py
@@ -1,31 +1,23 @@
+"""
+URL configuration for employee_project project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
 from django.contrib import admin
-from django.urls import path, include
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-
-# Template chart view import
-from performance.views import performance_chart
-
-schema_view = get_schema_view(
-   openapi.Info(
-      title="Employee Management API",
-      default_version='v1',
-      description="APIs for Django Internship Project",
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
-)
+from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('employees.urls')),
-    path('api/', include('attendance.urls')),
-    path('api/', include('performance.urls')),
-
-    # Swagger UI
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-
-    # Chart page URL
-    path('charts/', performance_chart, name='performance-chart'),
+    path("admin/", admin.site.urls),
 ]
